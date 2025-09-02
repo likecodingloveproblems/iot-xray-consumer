89241b5c1b0ef9ce4d3b1ed85fc7fc28f43786bf
mam <mamehdizadeh.official@gmail.com>
2025-09-02 13:08:28 +0330

    Update swagger doc

--- src/signals/signals.dto.ts
+++ src/signals/signals.dto.ts
@@ -1,14 +1,45 @@
-import { IsString, IsNumber, Min, IsObject, IsArray } from 'class-validator';
-import { Signal } from './schemas/signal';
+import {
+  IsString,
+  IsNumber,
+  Min,
+  IsObject,
+  IsArray,
+  ValidateNested,
+} from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { Type } from 'class-transformer';
+
+export class SignalData {
+  @ApiProperty({ example: 123 })
+  @IsNumber()
+  time: number;
+
+  @ApiProperty({ example: 1.5 })
+  @IsNumber()
+  x: number;
+
+  @ApiProperty({ example: 2.5 })
+  @IsNumber()
+  y: number;
+
+  @ApiProperty({ example: 0.8 })
+  @IsNumber()
+  speed: number;
+}
 
 export class CreateSignalDto {
+  @ApiProperty({ example: 'device-1' })
   @IsString()
   deviceId: string;
 
+  @ApiProperty({ example: 1693700000 })
   @IsNumber()
   @Min(0)
   timestamp: number;
 
+  @ApiProperty({ type: [SignalData] })
   @IsArray()
-  data: { time: number; x: number; y: number; speed: number }[];
+  @ValidateNested({ each: true })
+  @Type(() => SignalData)
+  data: SignalData[];
 }
